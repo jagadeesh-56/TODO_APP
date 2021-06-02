6b0293eca6d9e3d082ad09eea28f5c2625005179
Jagadeeshreddy <63099762+jagadeesh-56@users.noreply.github.com>
2021-06-02 12:42:19 +0530

    Add files via upload

---
+++ TodoForm.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class TodoForm extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             todotext:""
+        }
+    }
+    addtodo=(e)=>
+    {
+        e.preventDefault();
+        this.props.addTodo(this.state.todotext);
+        this.setState({todotext:''})
+    }
+    render() {
+        return (
+            <div>
+               <form onSubmit={this.addtodo}>
+                <input
+						type='text'
+						value={this.state.todotext}
+						onChange={(e) => this.setState({ todotext: e.target.value })}
+						placeholder='Add your todo here...'
+					/>
+                    </form>
+            </div>
+        )
+    }
+}
+export default TodoForm;
